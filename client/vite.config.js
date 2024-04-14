e072dfbabc60bf8d8aa0d85ba364317656f84ca5
janglee83 <giangle602@gmail.com>
2024-04-14 22:52:03 +0700

    update vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,20 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-    return {
-        plugins: [vue()],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
-            }
-        }
-    };
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      host: true,
+      hmr: {
+        host: 'localhost',
+      },
+      open: true,
+      port: 3000,
+    },
+  };
 });
